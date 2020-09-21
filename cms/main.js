e41166f4c8d686ced987cf9d35cbe56143d8f206
Ria <ria000130@gmail.com>
2020-09-21 23:18:34 +0900

    microCMS投稿部分を変更

--- cms/main.js
+++ cms/main.js
@@ -8,6 +8,50 @@ const markdownItMeta = require('markdown-it-meta')
 
 const filePath = process.argv[2]
 
+const host = 'https://ria.microcms.io/api/v1'
+
+const headers = {
+  'X-WRITE-API-KEY': process.env.X_WRITE_API_KEY,
+  'X-API-KEY': process.env.X_API_KEY,
+  'Content-Type': 'application/json',
+}
+
+const fetchTagIds = async (articleTags) => {
+  const fetchTagList = async () => {
+    const response = await fetch(`${host}/tag`, {
+      headers,
+    })
+    const data = await response.json()
+
+    return data.contents.map((tag) => ({ id: tag.id, name: tag.name }))
+  }
+
+  const postNewTag = async (tagName) => {
+    const response = await fetch(`${host}/tag`, {
+      headers,
+      method: 'post',
+      body: JSON.stringify({ name: tagName }),
+    })
+    const data = await response.json()
+
+    return data.id
+  }
+
+  const tagList = await fetchTagList()
+
+  return await Promise.all(
+    articleTags.map(async (tagName) => {
+      // すでに存在すれば id を返す
+      const TagId = tagList.find((tag) => tag.name === tagName)?.id
+      if (TagId) return TagId
+
+      // 存在しなければ新しく作成して id を返す
+      const newTagId = await postNewTag(tagName)
+      return newTagId
+    })
+  )
+}
+
 const main = async (filePath) => {
   fs.readFile(filePath, 'utf-8', async (err, article) => {
     if (err) throw err
@@ -16,27 +60,28 @@ const main = async (filePath) => {
     md.render(article)
     const { meta } = md
 
+    const tags = await fetchTagIds(meta.topics)
+
     const json = {
       title: meta.title,
       description: meta.description,
-      tags: meta.topics.map((topic) => ({
-        fieldId: 'tag',
-        name: topic,
-      })),
+      tags,
       markdown: article,
     }
 
     const id = filePath.match(/\.\.\/articles\/(.+)\.md/)[1]
-    if (!id) return
 
-    await fetch(`https://ria.microcms.io/api/v1/article/${id}`, {
-      headers: {
-        'X-WRITE-API-KEY': process.env.X_WRITE_API_KEY,
-        'Content-Type': 'application/json',
-      },
-      method: 'put',
+    // 記事が存在すれば patch で更新、存在しなければ put で保存
+    const response = await fetch(`${host}/article/${id}`, { headers })
+    const exists = !!await response.json()
+    const method = exists ? 'patch' : 'put'
+
+    await fetch(`${host}/article/${id}`, {
+      headers,
+      method,
       body: JSON.stringify(json),
     })
+
   })
 }
 main(filePath)
